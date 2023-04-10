e6b379ca1c0f6ecf9f78838281bd19dddf5edd9f
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-04-10 11:25:13 -0500

    Fetch data from API and display it on console

--- src/components/rockets.jsx
+++ src/components/rockets.jsx
@@ -1,5 +1,20 @@
-const Rockets = () => (
-  <h1>Rockets</h1>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getRockets from '../Redux/Rockets/actions';
+
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  console.log(rockets);
+
+  return (
+    <h1>Rockets OwO</h1>
+  );
+};
 
 export default Rockets;
