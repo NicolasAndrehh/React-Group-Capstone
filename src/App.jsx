f1978589e4c92f5e8e6b5cc53ce8cd8289bfcfed
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-04-10 16:00:50 -0500

    Display reserved rockets in profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Header from './components/header';
 import Profile from './components/profile';
 import Rockets from './components/rockets';
 import Missions from './components/missions';
+import getRockets from './Redux/Rockets/actions';
 
 function App() {
+  const dispatch = useDispatch();
+
+  // Fecth Rockets once
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
