f16010f38ab8e6df387fb5202e4f7e73b5366495
Jaime Villegas <jaimevillegas296@gmail.com>
2023-04-11 17:10:01 -0500

    Update missionsActions.js - Delete comments

--- src/Redux/Missions/missionsActions.js
+++ src/Redux/Missions/missionsActions.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-// import { useSelector } from 'react-redux';
 
 const URL = 'https://api.spacexdata.com/v3/missions';
 
