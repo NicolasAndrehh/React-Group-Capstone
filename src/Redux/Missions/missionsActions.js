98694c19aa09045889eeab70c6ade284ab7f638c
Jaime Villegas <jaimevillegas296@gmail.com>
2023-04-11 16:03:35 -0500

    Work on missionsActions: Filter data

--- src/Redux/Missions/missionsActions.js
+++ src/Redux/Missions/missionsActions.js
@@ -3,10 +3,28 @@ import axios from 'axios';
 
 const URL = 'https://api.spacexdata.com/v3/missions';
 
+const transformData = (data) => {
+  const transformedData = [];
+
+  data.forEach((obj) => {
+    const newObj = {
+      mission_id: obj.mission_id,
+      mission_name: obj.mission_name,
+      description: obj.description,
+      reserved: false,
+    };
+
+    transformedData.push(newObj);
+  });
+
+  return transformedData;
+};
+
 const getMissions = createAsyncThunk('missions/getMissions', async () => {
   try {
     const response = await axios.get(URL);
-    return response.data;
+    const data = transformData(response.data);
+    return data;
   } catch (error) {
     return error;
   }
